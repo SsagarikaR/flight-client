5312aa4d9667e088ba6e0b61d473635dfed69be7
SsagarikaR <Sagarikar@mindfiresolutions.com>
2025-07-21 17:29:59 +0530

    fix: fix the routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 // src/App.tsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Status from "./pages/Status";
-import StatusDetail from "./pages/StatusDetail";
+// import StatusDetail from "./pages/StatusDetail";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/status" element={<Status />} />
-        <Route path="/status/:id" element={<StatusDetail />} />
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/status" element={<Status />} />
+    //     <Route path="/status/:id" element={<StatusDetail />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <Status />
+    </div>
   );
 };
 
