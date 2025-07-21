9072dca8e03cdcb96ae93b7932afaf32dc865e29
SsagarikaR <Sagarikar@mindfiresolutions.com>
2025-07-21 17:38:25 +0530

    fix: fix the vite build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 // src/App.tsx
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Status from "./pages/Status";
-// import StatusDetail from "./pages/StatusDetail";
+import StatusDetail from "./pages/StatusDetail";
 
 const App = () => {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/status" element={<Status />} />
-    //     <Route path="/status/:id" element={<StatusDetail />} />
-    //   </Routes>
-    // </BrowserRouter>
-    <div>
-      <Status />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/status" element={<Status />} />
+        <Route path="/status/:id" element={<StatusDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
